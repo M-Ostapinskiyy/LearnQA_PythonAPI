535579e988b8fbca69c551b808a44c23fa883373
M-Ostapinski <ostapinskiyy@gmail.com>
2025-07-29 23:40:02 +0400

    pam-pam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-print('Hello from Misha!')
+payload = {"name": "User"}
 
-response = requests.get('https://playground.learnqa.ru/api/get_text')
+response = requests.get('https://playground.learnqa.ru/api/hello', params = payload)
 print(response.text)
