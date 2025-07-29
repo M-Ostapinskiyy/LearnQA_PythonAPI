86195a9eb614f1a551ffbde7429c8192906b8c17
M-Ostapinski <ostapinskiyy@gmail.com>
2025-07-29 23:59:19 +0400

    pam-pam-2

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "User"}
+response = requests.get('https://playground.learnqa.ru/api/hello', params = {"name": "User "})
+parsed_response_text = response.json()
+print(parsed_response_text["answer"])
 
-response = requests.get('https://playground.learnqa.ru/api/hello', params = payload)
+response = requests.get('https://playground.learnqa.ru/api/get_text')
 print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a JSON format")
